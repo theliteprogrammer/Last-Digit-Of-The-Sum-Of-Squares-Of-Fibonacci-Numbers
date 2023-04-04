2d6303764129afdb2d876cbd62ee7b7a8f683957
theliteprogrammer <219037801@student.uj.ac.za>
2023-04-04 02:05:51 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
@@ -22,28 +23,28 @@ public class Main {
      * @param n - The last nth term of the sequence
      * @return Last Digit of The Sum Of Squares Of Fibonacci Numbers
      */
-    private static long getFibonacciSumSquaresFast(long n){
+    private static BigInteger getFibonacciSumSquaresFast(long n){
         if(n <= 1){
-            return n;
+            return BigInteger.valueOf(n);
         } else {
             // create 1D array
-            long[] array = new long[(int)n + 1];
-            array[0] = 0;
-            array[1] = 1;
-            long sum = 1; // array[0] + array[1]
+            BigInteger[] array = new BigInteger[(int)n + 1];
+            array[0] = BigInteger.valueOf(0);
+            array[1] = BigInteger.valueOf(1);
+            BigInteger sum = BigInteger.valueOf(1); // array[0] + array[1]
             // iterate through each term and calculate the fibonacci number
             for(int i = 2; i < array.length; i++){
-                array[i] = array[i - 2] + array[i - 1];
-                sum += Math.pow(array[i], 2);
+                array[i] = array[i - 2].add(array[i - 1]);
+                sum = sum.add(array[i].pow(2));
             }
-            return sum % 10;
+            return sum.mod(BigInteger.valueOf(10));
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
-        long s = getFibonacciSumSquaresFast(n);
+        BigInteger s = getFibonacciSumSquaresFast(n);
         System.out.println(s);
     }
 }
